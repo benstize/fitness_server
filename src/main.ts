9fd429f78841e5fd04691bb40ed90f43e521f966
stan <stanislav.benet@gmail.com>
2024-08-12 21:47:01 +0100

    check deployment

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
-  
   const config = new DocumentBuilder()
     .setTitle('Fitness Ben Swagger')
     .setDescription('Sport API')
